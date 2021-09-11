fa9558e9516ffeb332ecd01a07075e699612a2b3
Nico Stranquist <nstranquist@gmail.com>
2021-09-11 16:03:46 -0500

    get makefile and main() function created

--- Makefile
+++ Makefile
@@ -1,1 +1,5 @@
 # Makefile - Project 1
+# https://cs.colby.edu/maxwell/courses/tutorials/maketutor/
+
+driver: driver.c
+	gcc -o driver driver.c -I .
--- driver.c
+++ driver.c
@@ -1,2 +1,53 @@
 // driver.c
 
+#include <time.h>
+#include <stdio.h>
+
+typedef struct data_struct {
+  time_t time; // Time stamp
+  char type; // Message type (I/W/E/F)
+  char * string; // Message string
+} data_t;
+
+int addmsg (const char type, const char * msg);
+void clearlog();
+char * getlog();
+int savelog(char * filename);
+
+int main(int argc, char** argv) {
+  printf("Hello, World!\n");
+
+  return 0;
+}
+
+// The function addmsg creates the data structure data_t by adding the time stamp to the supplied parameters message type
+// and message string, and inserts a copy of the data structure at the end of the list. It also verifies that the message type is a
+// valid message type and issues an error if it is invalid.
+int addmsg (const char type, const char * msg) {
+
+}
+
+/**
+ * The savelog function saves the logged message to a disk file.
+ * 
+ */
+int savelog(char * filename) {
+
+}
+
+/**
+ * The
+ * clearlog function releases all the storage that has been allocated for the logged message and empties the list of logged
+ * messages
+ */
+char * getlog() {
+
+}
+
+/**
+ * The getlog function allocates enough space for a string containing the entire log, copies the log into this string,
+ * and returns a pointer to the string. It is the responsibility of the calling program to free this memory when necessary.
+ */
+void clearlog () {
+
+}
